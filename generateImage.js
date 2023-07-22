8e74fa126210947376ace90f543be4b794eb6355
W4STT3D <80830662+W4STT3D@users.noreply.github.com>
2023-07-22 13:49:04 +0200

    Final bot code

---
+++ generateImage.js
@@ -0,0 +1,78 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+const background = "https://i.imgur.com/8lWlHGt.png"
+
+const dim = {
+    height: 500,
+    width: 1100,
+    margin: 50
+}
+
+const av = {
+    size: 256,
+    x: 420,
+    y: 80
+}
+
+
+
+const generateImage = async (member, server) => {
+    let username = member.user.username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({extension: "png", dynamic: false, size: av.size})
+
+    const canvas = Canvas.createCanvas(dim.width, dim.height)
+    const ctx = canvas.getContext("2d")
+
+    // draw in the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+
+    // draw black tinted box
+    ctx.fillStyle = "rgba(0,0,0,0.8)"
+    ctx.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin)
+
+    const avimg = await Canvas.loadImage(avatarURL)
+    ctx.save()
+    
+    // draw white border around the avatar
+    const borderWidth = 10; // adjust the border width as needed
+    const avatarCenterX = av.x + av.size / 2;
+    const avatarCenterY = av.y + av.size / 2;
+    ctx.beginPath();
+    ctx.arc(avatarCenterX, avatarCenterY, av.size / 2 + borderWidth, 0, Math.PI * 2, true);
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(avatarCenterX, avatarCenterY, av.size / 2, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    // draw the avatar image
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore()
+
+    // write in text
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+
+    // // draw in Welcome
+    // ctx.font = "50px Roboto"
+    // ctx.fillText("Welcome", dim.width/2, dim.margin + 70)
+
+    // draw in to the server
+    ctx.font = "40px Roboto"
+    ctx.fillText(username + " just joined the server", dim.width / 2, dim.height - dim.margin - 50)
+
+    // draw member count
+    const memberCount = server.memberCount.toString();
+    ctx.font = "30px Roboto";
+    ctx.fillStyle = "grey";
+    ctx.fillText("Member #" + memberCount, dim.width / 2, dim.height - dim.margin - 15);
+
+    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), "welcome.png")
+    return attachment
+}
+
+module.exports = generateImage
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 require("dotenv").config()
+
+const generateImage = require("./generateImage")
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+    ],
+   partials: [Partials.Channel] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello World!!!!!")
+    }
+})
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member, member.guild);
+    member.guild.channels.cache.get(welcomeChannelId).send({
+      content: `Welcome <@${member.id}> to Dyl's Den!`,
+      files: [img],
+    });
+  });
+
+const welcomeChannelId = "1131380261158912141"
+
+client.login(process.env.TOKEN)
